272f8d1196ae4b9d34ceed80dc0034d09f0b232d
axell-brendow <breno.axel@gmail.com>
2021-02-05 23:26:47 -0300

    Add Mongo to docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo 'export PATH="$PATH:$HOME/.dotnet/tools"' >> ~/.bashrc
 export PATH="$PATH:$HOME/.dotnet/tools"
 
 # Try to connect to the database every 1 second over 120 seconds
-dockerize -wait tcp://$DB_HOST:$DB_PORT -timeout 120s
+dockerize -wait tcp://$SQL_SERVER_DB_HOST:$SQL_SERVER_DB_PORT -wait tcp://$MONGO_DB_HOST:$MONGO_DB_PORT -timeout 120s
 
 dotnet ef database update
 popd
